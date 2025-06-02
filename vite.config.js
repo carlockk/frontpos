9f95b640c60ce21e32e79e371885c343489d2aff
carlockk <carlos.virtualdesk@gmail.com>
2025-06-02 01:12:58 -0400

    vite corregido para vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // 🔥 Para redirigir todas las rutas a index.html
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
